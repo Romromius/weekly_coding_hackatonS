cbddf9b95eb6da882633f93211360b601ca3ce8e
Romromius <118234421+Romromius@users.noreply.github.com>
2024-01-15 00:06:17 +1000

    Added background

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ COLORS = {'q': 'cyan', 'w': 'green', 'e': 'red', 'r': 'yellow',
 GRAVITY = 9.8
 LEVEL = 2
 
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image):
+        super().__init__()
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.target = 0
+
+
+def lerp(x, y, s):
+    return x * (1 - s) + y * s
+
+
 def load_level(level):
     global music
     global voices
@@ -77,9 +91,9 @@ class ParticleText():
 
     def update(self, tick=0.1):
         self.velocity[1] += self.gravity
-        self.x += self.velocity[0] / 7000
-        self.y += self.velocity[1] / 7000
-        if self.pos not in resolution:
+        self.x += self.velocity[0] / 7000 * clock.get_time()/2
+        self.y += self.velocity[1] / 7000 * clock.get_time()/2
+        if self.pos not in RESOLUTION:
             del self
 
     def render(self, surface):
@@ -89,9 +103,9 @@ class ParticleText():
 # ЫГРА
 
 pygame.init()
-resolution = width, height = 800, 600
+RESOLUTION = WIDTH, HEIGHT = 800, 600
 pygame.init()
-screen = pygame.display.set_mode(resolution)
+screen = pygame.display.set_mode(RESOLUTION)
 clock = pygame.time.Clock()
 pygame.font.init()
 my_font = pygame.font.SysFont('Comic Sans MS', 30)
@@ -113,9 +127,26 @@ do_voice = True
 voice_vol = 1
 pygame.mixer.Channel(2).set_volume(0.5)
 
+all_sprites = pygame.sprite.Group()
+background = Background('data\\sprites\\default_room.png')
+all_sprites.add(background)
+
 running = True
 while running:
+    screen.fill('black')
+    all_sprites.draw(screen)
+    match background.target:
+        case 0:
+            background.rect.center = (WIDTH / 2, HEIGHT / 2)
+        case 1:
+            background.rect.x = pygame.math.lerp(background.rect.x, -200, 0.02)
+            background.rect.y = pygame.math.lerp(background.rect.y, -100, 0.02)
+        case 2:
+            background.rect.x = pygame.math.lerp(background.rect.x, 0, 0.02)
+            background.rect.y = pygame.math.lerp(background.rect.y, 0, 0.02)
+    print(background.rect.x)
 
+    # Ивенты
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -133,6 +164,7 @@ while running:
                 load_level(LEVEL)
                 score = 0
 
+
         # Инпут
         if event.type == pygame.KEYDOWN:
             match event.dict['unicode']:
@@ -148,6 +180,7 @@ while running:
                 case 'r' | 'к':
                     player_input.add('к')
                     input_list.add('к')
+
         if event.type == pygame.KEYUP:
             match event.dict['unicode']:
                 case 'q' | 'й':
@@ -159,15 +192,15 @@ while running:
                 case 'r' | 'к':
                     input_list.remove('к')
 
+
+
     # Игровой цик
     pygame.mixer.Channel(1).set_volume(voice_vol)
     if do_voice:
         voice_vol = 1
     else:
         voice_vol = 0
-    t += clock.tick() / 1000
-    screen.fill('black')
-    pygame.draw.line(screen, 'red', (0, 100), (width, 100))
+    pygame.draw.line(screen, 'red', (0, 100), (WIDTH, 100))
     if play:
         for i in notes:
             for j in notes[i]['note']:
@@ -175,20 +208,24 @@ while running:
                     if not notes[i]['done']:
                         notes[i]['done'] = True
                         do_voice = True
+                        background.target = 2
                 if j in 'йцукен':
                     if not notes[i]['done']:
                         if cord(i[0], t) + 100 < 75:  # Таймаут
+                            background.target = 1
                             if i[0] != i[1]:
                                 pygame.mixer.Channel(2).play(SOUNDS['note_hold'])
                             else:
                                 pygame.mixer.Channel(2).play(SOUNDS['note_miss'])
                             do_voice = False
                             score -= 200
-                            marks.append(ParticleText((450, 300), random.uniform(-4000, 4000), -3000,
-                                                      random.choice('Поздно/Не успел.../Ты опоздал!/Слишком долго.../ПРОСНИСЬ!/Не спи'.split('/'))))
+                            marks.append(ParticleText((450, 300), random.uniform(-4000, 4000), -2000,
+                                                      random.choice('Поздно/Не успел.../Ты опоздал!/Слишком долго...'
+                                                                    '/ПРОСНИСЬ!/Не спи'.split('/'))))
 
                             notes[i]['done'] = True
                         if 75 < cord(i[0], t) + 100 < 125:  # Попадание
+                            background.target = 1
                             if player_input:
                                 if player_input & set(j):
                                     score += count_score(cord(i[0], t))
@@ -199,13 +236,14 @@ while running:
                                     if 95 < cord(i[0], t) + 100 < 105:
                                         mark = random.choice('ИДЕАЛЬНО/В точку!/Ай да молодец!/16 Мегабайт!'.split('/'))
                                     marks.append(ParticleText((450, 300),
-                                                              random.uniform(-4000, 4000), -3000, mark))
+                                                              random.uniform(-4000, 4000), -2000, mark))
                                 else:  # Промах
                                     score -= 100
                                     do_voice = False
                                     marks.append(ParticleText((450, 300),
-                                                              random.uniform(-4000, 4000), -3000,
-                                                              random.choice('Мимо/Как так?/SyntaxError/Разуй глаза/Ну ты чё'.split('/'))))
+                                                              random.uniform(-4000, 4000), -2000,
+                                                              random.choice('Мимо/Как так?/SyntaxError/'
+                                                                            'Разуй глаза/Ну ты чё'.split('/'))))
                                     if i[0] != i[1]:
                                         pygame.mixer.Channel(2).play(SOUNDS['note_mishold'])
                                     else:
@@ -217,28 +255,28 @@ while running:
                     match j:
                         case 'q':
                             offset = 50
-                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, height))
+                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, HEIGHT))
                         case 'w':
                             offset = 125
-                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, height))
+                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, HEIGHT))
                         case 'e':
                             offset = 200
-                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, height))
+                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, HEIGHT))
                         case 'r':
                             offset = 275
-                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, height))
+                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, HEIGHT))
                         case 'й':
                             offset = 50 + 450
-                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, height))
+                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, HEIGHT))
                         case 'ц':
                             offset = 125 + 450
-                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, height))
+                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, HEIGHT))
                         case 'у':
                             offset = 200 + 450
-                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, height))
+                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, HEIGHT))
                         case 'к':
                             offset = 275 + 450
-                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, height))
+                            pygame.draw.line(screen, 'grey', (offset, 0), (offset, HEIGHT))
                         case None:
                             continue
                     if i[0] != i[1] and cord(i[1], t) > 0:
@@ -259,3 +297,5 @@ while running:
         i.render(screen)
     pygame.display.flip()
     player_input.clear()
+    clock.tick(120)
+    t += clock.get_time() / 1000
